8ddaf522e0804ca58476e85a5c1db0c99f7fb30c
Adam Venturella <aventurella@gmail.com>
2014-07-29 15:59:40 -0700

    Update init.js

--- rich/init.js
+++ rich/init.js
@@ -26,7 +26,6 @@ define(function (require, exports, module) {
     };
 
     // shortcuts
-    // zap
     exports.View = view.FamousView;
     exports.ItemView = itemview.FamousItemView;
     exports.LayoutView = layouts.FamousLayoutView;
