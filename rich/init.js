c9a393af7fb5db900e86469ed94fca06836ef988
Adam Venturella <aventurella@gmail.com>
2014-07-29 15:18:59 -0700

    remove BAR comment

--- rich/init.js
+++ rich/init.js
@@ -26,7 +26,6 @@ define(function (require, exports, module) {
     };
 
     // shortcuts
-    // BAR
     exports.View = view.FamousView;
     exports.ItemView = itemview.FamousItemView;
     exports.LayoutView = layouts.FamousLayoutView;
