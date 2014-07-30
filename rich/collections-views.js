2eba0178d2957a4a2e4330f5b54bdd9b0e72bff9
dinopetrone <dinopetrone@gmail.com>
2014-07-30 12:05:36 -0700

    collectionview reset fix

--- rich/collections-views.js
+++ rich/collections-views.js
@@ -53,11 +53,12 @@ define(function (require, exports, module) {
 
         _famousReset: function(){
             this.root = null;
-            this.invalidate();
+            this.triggerRichInvalidate();
         },
 
         render: function(){
             if(!this.root || this.needsDisplay()){
+
                 this._ensureViewIsIntact();
                 this.triggerMethod('before:render', this);
                 this._renderChildren();
