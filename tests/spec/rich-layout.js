c0a5443fc3a38437604634c8fcbb51ec797ce279
Adam Venturella <aventurella@gmail.com>
2014-07-31 14:16:56 -0700

    beforeeach

--- tests/spec/rich-layout.js
+++ tests/spec/rich-layout.js
@@ -3,9 +3,14 @@ define(function(require, exports, module) {
 // Imports
 
 var rich = require('rich');
+
 //var _   = require('underscore');
 
 describe('Layout:', function() {
+
+    beforeEach(function() {
+        console.log(22);
+    });
 
     function newLayout(){
         var ConstraintLayout = rich.LayoutView.extend({
@@ -13,6 +18,8 @@ describe('Layout:', function() {
         });
         return new ConstraintLayout();
     }
+
+
 
     it('does this action', function(){
         var layout = newLayout();
