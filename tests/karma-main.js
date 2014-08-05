1eceff2068385fb0374cc9b95db8c9f73c6f861d
Adam Venturella <aventurella@gmail.com>
2014-08-05 13:39:12 -0700

    autolayout modifiers

--- tests/karma-main.js
+++ tests/karma-main.js
@@ -3,7 +3,8 @@ var tests = [
     // Load mocks and vendor init
     //'tests/mocks/init',
 
-    'tests/spec/rich-autolayout',
+    //'tests/spec/rich-autolayout',
+    'tests/spec/rich-autolayout-modifiers',
     // 'tests/spec/rich-layout',
     // 'tests/spec/rich-region',
     // 'tests/spec/rich-utils',
