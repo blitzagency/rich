9d1031928d17d02c235fabef2217084e4dfb6406
Adam Venturella <aventurella@gmail.com>
2014-08-12 14:01:16 -0700

    adds VFL tests

---
+++ tests/spec/rich-vfl.js
@@ -0,0 +1,21 @@
+define(function(require, exports, module) {
+
+// Imports
+
+var $ = require('jquery');
+var rich = require('rich');
+var Engine = require('famous/core/Engine');
+var Modifier = require('famous/core/Modifier');
+var Transform = require('famous/core/Transform');
+var Rectangle = require('app/shared/models/rectangle').Rectangle;
+var RectangleView = require('app/shared/views/rectangle-view').RectangleView;
+
+describe('Visual Format Language:', function() {
+
+    it('converts simple to JSON', function(){
+
+
+    });
+
+}); // eof describe
+}); // eof define
