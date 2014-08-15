f29b123ca643de72a16970b606a5740c07c90a26
Adam Venturella <aventurella@gmail.com>
2014-08-15 16:48:00 -0700

    adds removeConstraint(s) and tests

--- tests/spec/rich-view-constraints.js
+++ tests/spec/rich-view-constraints.js
@@ -199,5 +199,160 @@ describe('View + Constraints:', function() {
         };
     });
 
+    it('removes constraint', function(done){
+        var color0 = new Rectangle({
+            color: colors[0]
+        });
+
+        var color1 = new Rectangle({
+            color: 'red'
+        });
+
+        var box0 = new RectangleView({
+            model: color0,
+        });
+
+        var box1 = new RectangleView({
+            model: color1,
+        });
+
+        box0.name = 'box0';
+        box1.name = 'box1';
+
+        box0.box1 = box1;
+        box0.addSubview(box1);
+
+        region.show(box0);
+
+
+        box0.onShow = function(){
+
+            expect(box0._autolayout.left.value).toBe(0);
+            expect(box0._autolayout.right.value).toBe(0);
+            expect(box0._autolayout.width.value).toBe(1000);
+            expect(box0._autolayout.height.value).toBe(800);
+
+            expect(box1._autolayout.left.value).toBe(0);
+            expect(box1._autolayout.right.value).toBe(0);
+            expect(box1._autolayout.width.value).toBe(1000);
+            expect(box1._autolayout.height.value).toBe(800);
+
+            expect(css.getSize(box1.$el)).toEqual([1000, 800]);
+
+            var c1 = constraints.constraintWithJSON({
+                item: box1,
+                attribute: 'width',
+                relatedBy: '==',
+                constant: 500
+            });
+
+            var c2 = constraints.constraintWithJSON({
+                item: box1,
+                attribute: 'height',
+                relatedBy: '==',
+                constant: 200
+            });
+
+            box0.addConstraints([c1, c2]);
+
+            render().then(function(){
+                expect(box1._autolayout.width.value).toBe(500);
+                expect(box1._autolayout.height.value).toBe(200);
+                expect(box1._autolayout.bottom.value).toBe(600);
+                expect(css.getSize(box1.$el)).toEqual([500, 200]);
+
+                box0.removeConstraint(c1);
+
+                render().then(function(){
+                    expect(box1._autolayout.width.value).toBe(1000);
+                    expect(box1._autolayout.height.value).toBe(200);
+
+                    box0.removeConstraint(c2);
+
+                    render().then(function(){
+                        expect(box1._autolayout.width.value).toBe(1000);
+                        expect(box1._autolayout.height.value).toBe(800);
+                        done();
+                    });
+                });
+
+            });
+        };
+    });
+
+    it('removes constraints', function(done){
+        var color0 = new Rectangle({
+            color: colors[0]
+        });
+
+        var color1 = new Rectangle({
+            color: 'red'
+        });
+
+        var box0 = new RectangleView({
+            model: color0,
+        });
+
+        var box1 = new RectangleView({
+            model: color1,
+        });
+
+        box0.name = 'box0';
+        box1.name = 'box1';
+
+        box0.box1 = box1;
+        box0.addSubview(box1);
+
+        region.show(box0);
+
+
+        box0.onShow = function(){
+
+            expect(box0._autolayout.left.value).toBe(0);
+            expect(box0._autolayout.right.value).toBe(0);
+            expect(box0._autolayout.width.value).toBe(1000);
+            expect(box0._autolayout.height.value).toBe(800);
+
+            expect(box1._autolayout.left.value).toBe(0);
+            expect(box1._autolayout.right.value).toBe(0);
+            expect(box1._autolayout.width.value).toBe(1000);
+            expect(box1._autolayout.height.value).toBe(800);
+
+            expect(css.getSize(box1.$el)).toEqual([1000, 800]);
+
+            var c1 = constraints.constraintWithJSON({
+                item: box1,
+                attribute: 'width',
+                relatedBy: '==',
+                constant: 500
+            });
+
+            var c2 = constraints.constraintWithJSON({
+                item: box1,
+                attribute: 'height',
+                relatedBy: '==',
+                constant: 200
+            });
+
+            box0.addConstraints([c1, c2]);
+
+            render().then(function(){
+                expect(box1._autolayout.width.value).toBe(500);
+                expect(box1._autolayout.height.value).toBe(200);
+                expect(box1._autolayout.bottom.value).toBe(600);
+                expect(css.getSize(box1.$el)).toEqual([500, 200]);
+
+                box0.removeConstraints([c1, c2]);
+
+                render().then(function(){
+                    expect(box1._autolayout.width.value).toBe(1000);
+                    expect(box1._autolayout.height.value).toBe(800);
+                    done();
+                });
+
+            });
+        };
+    });
+
 }); // eof describe
 }); // eof define
