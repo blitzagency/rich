67f61a7dafaf2267c99807b6193e6bc7b99fdae2
Adam Venturella <aventurella@gmail.com>
2014-08-20 05:16:29 -0700

    adds sizeForViewAtIndex and intrinsic size tests

--- tests/spec/rich-collectionview.js
+++ tests/spec/rich-collectionview.js
@@ -12,6 +12,8 @@ var RectangleView = require('app/shared/views/rectangle-view').RectangleView;
 var render = require('tests/utils/time').render;
 var wait = require('tests/utils/time').wait;
 var colors = require('tests/utils/colors').blue;
+var css = require('tests/utils/css');
+var matrix = require('tests/utils/matrix');
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
@@ -43,58 +45,84 @@ describe('Auto Layout:', function() {
         root = null;
     });
 
-    // xit('sets explicit size on subview', function(done){
-    //     var model = new Rectangle();
+    it('display views from initial collection (sizeForViewAtIndex)', function(done){
 
-    //     var view = new RectangleView({
-    //         model: model,
-    //         constraints: [
-    //             {
-    //                 item: 'navigation',
-    //                 attribute: 'width',
-    //                 relatedBy: '==',
-    //                 constant: 300,
-    //             },
+        var color0 = new Rectangle({
+            color: colors[7]
+        });
 
-    //             {
-    //                 item: 'navigation',
-    //                 attribute: 'height',
-    //                 relatedBy: '==',
-    //                 constant: 100,
-    //             }
-    //         ]
-    //     });
+        var color1 = new Rectangle({
+            color: colors[6]
+        });
 
-    //     // region.constraints = function(){
-    //     //     return [
-    //     //         'H:|[currentView]|',
-    //     //         'V:|[currentView]|',
-    //     //     ];
-    //     // };
+        var color2 = new Rectangle({
+            color: colors[5]
+        });
 
-    //     view.navigation = new RectangleView({
-    //         model: model,
-    //        // size: [100, 200]
-    //     });
+        var color3 = new Rectangle({
+            color: colors[4]
+        });
 
-    //     region.name = 'regionView';
-    //     view.name = 'rectangleView';
-    //     view.navigation.name = 'navigation';
 
-    //     view.addSubview(view.navigation);
-    //     region.show(view);
+        var collection = new backbone.Collection([
+            color0, color1, color2, color3]);
 
-    //     view.onShow = function(){
-    //         //expect(view.navigation.getSize()).toEqual([300, 200]);
-    //         console.log(view.navigation.getSize());
-    //         //done();
-    //     };
-    // });
 
-    it('works', function(done){
+        var collectionView = new rich.CollectionView({
+            collection: collection,
+            orientation: 'vertical',
+            childView: RectangleView,
+            spacing: 1,
 
-        var view = RectangleView.extend({
-            size: [100, 50]
+            sizeForViewAtIndex: function(view, index){
+                return [0, 20];
+            }
+        });
+
+        region.name = 'region';
+        collectionView.name = 'collectionView';
+
+
+        region.constraints = function(){
+            return [
+                'H:|[currentView]|',
+                'V:|[currentView]|',
+            ];
+        };
+
+        region.show(collectionView);
+
+        collectionView.onShow = function(){
+            var targetHeight = 20; // sizeForViewAtIndex value
+
+            expect($el.children().length).toEqual(4);
+            var startColor = 7;
+
+            _.each($el.children(), function(child, index){
+                var $child = $(child);
+                // http://jsperf.com/getting-first-child-element-using-jquery/3
+                var $rect = $($child.children()[0]);
+
+                var value = css.rgb2hex($rect.css('backgroundColor'));
+                var targetValue = colors[startColor - index];
+                var targetTranslation = (targetHeight + collectionView.spacing) * index;
+
+                expect($child.height()).toEqual(targetHeight);
+                expect(value).toEqual(targetValue);
+                expect(matrix.getTranslation($child).y).toEqual(targetTranslation);
+
+            });
+
+
+            done();
+        };
+
+    });
+
+    it('display views from initial collection (intrinsic size)', function(done){
+
+        var AltView = RectangleView.extend({
+            size: [0, 50]
         });
 
         var color0 = new Rectangle({
@@ -113,6 +141,7 @@ describe('Auto Layout:', function() {
             color: colors[4]
         });
 
+
         var collection = new backbone.Collection([
             color0, color1, color2, color3]);
 
@@ -120,8 +149,8 @@ describe('Auto Layout:', function() {
         var collectionView = new rich.CollectionView({
             collection: collection,
             orientation: 'vertical',
-            childView: view,
-            spacing: 1,
+            childView: AltView,
+            spacing: 5,
         });
 
         region.name = 'region';
@@ -138,13 +167,29 @@ describe('Auto Layout:', function() {
         region.show(collectionView);
 
         collectionView.onShow = function(){
-            //console.log(region.getSize());
-            //console.log(collectionView.getSize());
+            var targetHeight = 50; // sizeForViewAtIndex value
+
+            expect($el.children().length).toEqual(4);
+            var startColor = 7;
+
+            _.each($el.children(), function(child, index){
+                var $child = $(child);
+                // http://jsperf.com/getting-first-child-element-using-jquery/3
+                var $rect = $($child.children()[0]);
+
+                var value = css.rgb2hex($rect.css('backgroundColor'));
+                var targetValue = colors[startColor - index];
+                var targetTranslation = (targetHeight + collectionView.spacing) * index;
+
+                expect($child.height()).toEqual(targetHeight);
+                expect(value).toEqual(targetValue);
+                expect(matrix.getTranslation($child).y).toEqual(targetTranslation);
+
+            });
+
+
             done();
         };
-
-
-        // done();
 
     });
 
