ba1751f667586256784e7c502964f3ce2e56ddcf
dinopetrone <dinopetrone@gmail.com>
2014-08-22 09:34:01 -0700

    testing firefox travis

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox'],
     singleRun: false
 
   });
