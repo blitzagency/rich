9e029a1b42aea28438153f4b53dc3dcf3486ea6c
dinopetrone <dinopetrone@gmail.com>
2014-08-21 19:40:55 -0700

    travis

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,10 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    plugins: [
+        'karma-phantomjs-launcher',
+    ]
 
   });
 };
