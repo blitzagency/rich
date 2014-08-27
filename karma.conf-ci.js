97727aae9d1e8257a1a816f5e9ea159ed7b83bbe
dinopetrone <dinopetrone@gmail.com>
2014-08-27 15:50:49 -0700

    travis

---
+++ karma.conf-ci.js
@@ -0,0 +1,81 @@
+
+var fs = require('fs');
+
+module.exports = function(config) {
+
+  // Use ENV vars on Travis and sauce.json locally to get credentials
+  if (!process.env.SAUCE_USERNAME) {
+    if (!fs.existsSync('sauce.json')) {
+      console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
+      process.exit(1);
+    } else {
+      process.env.SAUCE_USERNAME = require('./sauce').username;
+      process.env.SAUCE_ACCESS_KEY = require('./sauce').accessKey;
+    }
+  }
+
+  // Browsers to run on Sauce Labs
+  var customLaunchers = {
+    'SL_Chrome': {
+      base: 'SauceLabs',
+      browserName: 'chrome'
+    }
+  };
+
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'requirejs'],
+    exclude: [
+        '**/karma.conf.js'
+    ],
+
+    // list of files / patterns to load in the browser
+    files: [
+        {pattern: 'tests/common.js', included: true},
+        {pattern: 'tests/fixtures/*.html', included: false},
+        {pattern: 'tests/fixtures/test.css', included: true},
+        {pattern: 'tests/spec/*.js', included: false},
+        {pattern: 'tests/vendor/**/*.js', included: false},
+        {pattern: 'tests/utils/*.js', included: false},
+        {pattern: 'rich/**/*.js', included: false},
+        {pattern: 'demos/src/static/js/**/*.js', included: false},
+        {pattern: 'demos/src/static/js/**/*.html', included: false},
+        {pattern: 'demos/src/static/css/styles.css', included: true},
+
+        'tests/karma-main.js'
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['dots', 'saucelabs'],
+
+
+    // web server port
+    port: 9876,
+
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    sauceLabs: {
+      testName: 'Rich and Famous'
+    },
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: Object.keys(customLaunchers),
+    singleRun: true
+  });
+};
