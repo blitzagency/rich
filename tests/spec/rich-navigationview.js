cae7c55d2af963f35e2827d8716a05e1d80c49bf
Adam Venturella <aventurella@gmail.com>
2014-09-10 07:56:44 -0700

    adds start of navigation controller tests

---
+++ tests/spec/rich-navigationview.js
@@ -0,0 +1,75 @@
+define(function(require, exports, module) {
+
+// Imports
+
+var _ = require('underscore');
+var $ = require('jquery');
+var rich = require('rich');
+var utils = require('rich/utils');
+var NavigationController = require('rich/controllers/navigation').NavigationController;
+var Modifier = require('famous/core/Modifier');
+var Transform = require('famous/core/Transform');
+var Rectangle = require('app/shared/models/rectangle').Rectangle;
+var RectangleView = require('app/shared/views/rectangle-view').RectangleView;
+var matrix = require('tests/utils/matrix');
+var css = require('tests/utils/css');
+var render = require('tests/utils/time').render;
+var colors = require('tests/utils/colors').blue;
+var Setup = require('tests/utils/setup').Setup;
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+
+describe('NavigationController:', function() {
+
+    beforeEach(function() {
+        loadFixtures('famous.html');
+
+    });
+
+    afterEach(function() {
+
+    });
+
+    xit('navigation view', function(done){
+        var context = new Setup(done);
+        var region = context.region;
+        var root = context.root;
+
+        var color0 = new Rectangle({
+            color: colors[7]
+        });
+
+        var color1 = new Rectangle({
+            color: colors[4]
+        });
+
+        var box0 = new RectangleView({
+            model: color0,
+        });
+
+        var box1 = new RectangleView({
+            model: color1,
+        });
+
+        var nav = new NavigationController();
+
+
+        root.fillWithSubview(nav);
+        nav.pushView(box0);
+
+        setTimeout(function(){
+            nav.pushView(box1);
+        }, 2000);
+
+        setTimeout(function(){
+            nav.popView();
+            context.done();
+        }, 4000);
+
+    });
+
+
+
+
+}); // eof describe
+}); // eof define
