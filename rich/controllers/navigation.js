d7e5daccac68a72f730b30b46a96caf35990fadd
Adam Venturella <aventurella@gmail.com>
2014-09-11 04:42:52 -0700

    working on navigation controller

--- rich/controllers/navigation.js
+++ rich/controllers/navigation.js
@@ -1,35 +1,50 @@
 define(function (require, exports, module) {
     var _ = require('underscore');
+    var rich = require('rich');
     var Easing = require('famous/transitions/Easing');
-    var View = require('../view').FamousView;
     var constraintWithJSON = require('rich/autolayout/constraints').constraintWithJSON;
     var utils = require('rich/utils');
 
-    var NavigationController = View.extend({
+    var NavigationController = rich.View.extend({
         topView: null,
         transitionDuration: 230,
         _views: null,
 
         constructor: function(options){
-            View.prototype.constructor.apply(this, arguments);
+            options || (options = {});
 
+            rich.View.prototype.constructor.apply(this, arguments);
             this._views = [];
+
+            if(options.view){
+                var view = options.view;
+                var constraints = this.preparePushConstraints(view);
+
+                this.prepareSubviewAdd(view);
+                this.preparePushView(view);
+
+                setTimeout(function(){
+                    console.log(this.getSize());
+                    this.addConstraints(constraints);
+                }.bind(this), 1000);
+                // utils.defer(function(){
+
+
+                // });
+            }
+        },
+
+        _initializeConstraints: function(){
+            //debugger;
+            rich.View.prototype._initializeConstraints.apply(this, arguments);
         },
 
         pushView: function(view){
             var outConstraint;
-            var inConstraint;
+            var inConstraint = this.preparePushInConstraint(view);
 
             if(this.topView){
-
-                outConstraint = constraintWithJSON({
-                    item: this.topView,
-                    attribute: 'right',
-                    relatedBy: '==',
-                    toItem: this,
-                    toAttribute: 'width',
-                    constant: 0
-                });
+                outConstraint = this.preparePushOutConstraint(this.topView);
 
                 var index = this._views.length - 1;
                 var tmp = this._views[index];
@@ -37,48 +52,12 @@ define(function (require, exports, module) {
                 tmp.positionConstraint = outConstraint;
             }
 
-            var c1 = constraintWithJSON({
-                item: view,
-                attribute: 'width',
-                relatedBy: '==',
-                toItem: this,
-                toAttribute: 'width',
-                constant: 0
-            });
+            var constraints = [].concat(
+                this.preparePushConstraints(view),
+                inConstraint
+            );
 
-            var c2 = constraintWithJSON({
-                item: view,
-                attribute: 'height',
-                relatedBy: '==',
-                toItem: this,
-                toAttribute: 'height',
-            });
-
-            inConstraint = constraintWithJSON({
-                item: view,
-                attribute: 'left',
-                relatedBy: '==',
-                toItem: this,
-                toAttribute: 'width',
-                constant: 0
-            });
-
-            var constraints = [].concat(c1, c2, inConstraint);
-
-            this._isPush = true;
-            this.topView = view;
-
-            view.navigationController = this;
-            view.autolayoutTransition = {
-                duration: this.transitionDuration,
-                curve: Easing.outQuad
-            };
-
-            this._views.push({
-                view: view,
-                positionConstraint: null
-            });
-
+            this.preparePushView(view);
             this.prepareSubviewAdd(view);
             this.addConstraints(constraints);
 
@@ -97,14 +76,7 @@ define(function (require, exports, module) {
                 var views = this._views;
                 var previousIndex = views.length - 2;
 
-                var outConstraint = constraintWithJSON({
-                    item: this.topView,
-                    attribute: 'left',
-                    relatedBy: '==',
-                    toItem: this,
-                    toAttribute: 'width',
-                    constant: 0
-                });
+                var outConstraint = this.preparePopOutConstraint(this.topView);
 
                 this.listenTo(this.topView, 'autolayoutTransition:complete', function(view, prop){
                     view.navigationController = null;
@@ -120,6 +92,76 @@ define(function (require, exports, module) {
                 this.addConstraint(outConstraint);
                 this.removeConstraint(views[previousIndex].positionConstraint);
             }
+        },
+
+        preparePushView: function(view){
+            this.topView = view;
+
+            view.navigationController = this;
+            view.autolayoutTransition = {
+                duration: this.transitionDuration,
+                curve: Easing.outQuad
+            };
+
+            this._views.push({
+                view: view,
+                positionConstraint: null
+            });
+        },
+
+        preparePushInConstraint: function(view){
+            return constraintWithJSON({
+                item: view,
+                attribute: 'left',
+                relatedBy: '==',
+                toItem: this,
+                toAttribute: 'width',
+                constant: 0
+            });
+        },
+
+        preparePushOutConstraint: function(view){
+            return constraintWithJSON({
+                item: view,
+                attribute: 'right',
+                relatedBy: '==',
+                toItem: this,
+                toAttribute: 'width',
+                constant: 0
+            });
+        },
+
+        preparePushConstraints: function(view){
+            var c1 = constraintWithJSON({
+                item: view,
+                attribute: 'width',
+                relatedBy: '==',
+                toItem: this,
+                toAttribute: 'width',
+                constant: 0
+            });
+
+            var c2 = constraintWithJSON({
+                item: view,
+                attribute: 'height',
+                relatedBy: '==',
+                toItem: this,
+                toAttribute: 'height',
+                constant: 0
+            });
+
+            return [c1, c2];
+        },
+
+        preparePopOutConstraint: function(view){
+            return constraintWithJSON({
+                item: view,
+                attribute: 'left',
+                relatedBy: '==',
+                toItem: this,
+                toAttribute: 'width',
+                constant: 0
+            });
         },
 
     });
